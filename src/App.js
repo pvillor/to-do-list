b91eb6f7e1c64207a84f664aea20e7e797e90b7d
pvillor <pvillor@gmail.com>
2023-01-18 16:34:58 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Routes from './routes/index'
+import GlobalStyle from './styles/global'
+
+const App = () => {
+  return (
+    <>
+    <GlobalStyle />
+    <Routes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/index.jsx
@@ -0,0 +1,7 @@
+import Home from "../pages/Home"
+
+const Routes = () => {
+    return <Home />
+}
+
+export default Routes
